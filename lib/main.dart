aff2837e1e8b0980e8fb9db03eb15cea1d4d389b
manel sakouhi <sakouhimanel862@gmail.com>
2024-09-11 23:39:32 +0100

    write comment <3

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//
+//i love you
 import 'binding.dart';
 import 'core/services/services.dart';
 import 'localization/changelocal.dart';
